20771e274b12d5a36083c08826f9ae107e558c35
kiran navghare <navgharekiran16@gmail.com>
2022-05-17 10:36:50 +0530

    welcome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserComponent,
+    ListUserComponent,
+    MainContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
